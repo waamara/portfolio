7c98c47f80a7e6d96acc098a175b32b41ad4b75c
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-04-13 14:27:32 +0100

    commit test

--- portfolio/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div className='text-amber-300 bg-amber-950 '>App</div>
-  )
-}
-
-export default https://tailwindcss.com/docs/installation/using-vitehttps://tailwindcss.com/docs/installation/using-vitehttps://tailwindcss.com/docs/installation/using-vitehttps://tailwindcss.com/docs/installation/using-vitehttps://tailwindcss.com/docs/installation/using-vitehttps://tailwindcss.com/docs/installation/using-vitehttps://tailwindcss.com/docs/installation/using-viteApp
