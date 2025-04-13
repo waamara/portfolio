c359fcb956cb55ec4259822e710c95dc7582b370
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-04-13 15:02:42 +0100

    added the home page assets

---
+++ portfolio/src/home.tsx
@@ -0,0 +1,13 @@
+import backgr from 'assets/backgr.jpg'
+
+const home = () => {
+    return (
+        <div style={{
+            backgroundImage: `url(${backgr})`
+        }}>
+
+        </div>
+    )
+}
+
+export default home
