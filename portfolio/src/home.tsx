4ac48e773ffca182f3ce0fedc20b7fa11f1d8be8
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-04-13 22:25:29 +0100

    final touche

--- portfolio/src/home.tsx
+++ portfolio/src/home.tsx
@@ -2,17 +2,18 @@ import Bgimg from "../../portfolio/src/assets/backgr.png"
 import Seepng from "../../portfolio/src/assets/see.png"
 import Dowlandpng from "../../portfolio/src/assets/dowland.png"
 import Mypic from "../../portfolio/src/assets/me.jpg"
-import Pointimg from "../../portfolio/src/assets/point.png" 
+import Pointimg from "../../portfolio/src/assets/point.png"
 import Foldericone from "../../portfolio/src/assets/folder.png"
 import Img from "../../portfolio/src/assets/img.png"
+import More from "../../portfolio/src/assets/More.png"
+import Fram from "../../portfolio/src/assets/Frame.png"
 
 const home = () => {
     return (
-        <div
-            style={{
-                backgroundImage: `url($Bgimg)`,
-            }}
-            className="text-white bg-gray-950   p-8">
+        <div style={{
+            backgroundImage: `url($Bgimg)`,
+        }}
+            className="text-white bg-gray-950 opacity-100  p-8">
             <p className="text-2xl font-semibold">Hello it's Aymen </p> <br />
             <p>I'm a frontend developer with a passion for crafting pixel-perfect, user-friendly interfaces. I believe in clean, efficient code and building responsive websites that provide the best user experience, no matter the device.</p>
             <div className='flex flex-col pt-4'>
@@ -45,7 +46,7 @@ const home = () => {
                     <img src={Pointimg} alt="icone" />
                     <p className="text-2xl">Available For work</p>
                 </div>
-                <div  className=" bg-[#C2F6FF08] p-2 rounded-md flex flex-row">
+                <div className=" bg-[#C2F6FF08] p-2 rounded-md flex flex-row">
                     <img src={Pointimg} alt="icone" />
                     <p className="text-2xl">Algeria</p>
                 </div>
@@ -57,27 +58,35 @@ const home = () => {
                     <img src={Foldericone} alt="folder-icone" />
                 </button>
             </div>
-            <div className=" flex flex-col p-10 mt-10 items-center justify-center bg-[#C2F6FF08] rounded-2xl">
-                <img src={Img} alt="img" />
+            <div className=" flex flex-col p-6 mt-10 items-center h-[200px]  bg-[#C2F6FF08] rounded-2xl">
+                <p className="font-semibold">My Skills</p>
                 <button className="bg-[#38ACFF40] p-2 rounded-md flex flex-row">
-                    <p>view works</p>
-                    <img src={Foldericone} alt="folder-icone" />
+                    <p>More About Me</p>
+                    <img src={More} alt="folder-icone" />
                 </button>
             </div>
+            <p className="text-2xl pt-8">Popular Repo</p>
             <div className=" flex flex-col p-10 mt-10 items-center justify-center bg-[#C2F6FF08] rounded-2xl">
-                <img src={Img} alt="img" />
-                <button className="bg-[#38ACFF40] p-2 rounded-md flex flex-row">
-                    <p>view works</p>
-                    <img src={Foldericone} alt="folder-icone" />
-                </button>
+                <div>
+                    <p>project-name-from-yr-repo</p>
+                    <p>Public</p>
+                </div>
+                <div>
+                    <p>simple note app using Javascript...</p>
+                </div>
+                <div>
+                    <p>Javascript </p>
+                    <p>4 </p>
+                </div>
             </div>
-            <div className=" flex flex-col p-10 mt-10 items-center justify-center bg-[#C2F6FF08] rounded-2xl">
-                <img src={Img} alt="img" />
-                <button className="bg-[#38ACFF40] p-2 rounded-md flex flex-row">
-                    <p>view works</p>
-                    <img src={Foldericone} alt="folder-icone" />
-                </button>
+            <div className=" flex flex-col p-4 mt-10 bg-[#C2F6FF08] rounded-2xl">
+                <div className="flex flex-row justify-between pb-6">
+                    <p className="text-xl">My Github Contribution</p>
+                    <div className=" bg-[#38ACFF40] rounded-2xl p-2"><p className="text-[#76C6FF]">2024</p></div>
+                </div>
+                <img src={Fram} alt="Github-contribution-img" />
             </div>
+
         </div>
     )
 }
