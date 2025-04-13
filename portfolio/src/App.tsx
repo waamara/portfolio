3bcac1362b78381155fd876ae0d4a5b7729daa83
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-04-13 15:58:29 +0100

    added the content and things to the home page

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,9 @@
+import Home from "../src/home"
+
+const App = () => {
+  return (
+    <Home></Home>
+  )
+}
+
+export default App
--- portfolio/src/home.tsx
+++ portfolio/src/home.tsx
@@ -1,11 +1,46 @@
-import backgr from 'assets/backgr.jpg'
+import React from 'react'
 
 const home = () => {
     return (
-        <div style={{
-            backgroundImage: `url(${backgr})`
-        }}>
+        <div>
+            <p>Hello it's Aymen </p> <br />
+            <p>I'm a frontend developer with a passion for crafting pixel-perfect, user-friendly interfaces. I believe in clean, efficient code and building responsive websites that provide the best user experience, no matter the device.</p>
+            <div className='flex flex-col'>
+                <div className='flex flex-row'>
+                    <p>Resume</p>
+                    <button>See cv </button>
+                    <button>Dowland Cv</button>
+                </div>
+                <div className='flex flex-row'>
+                    <p>Follow On</p>
+                    <a href="">Linkedin</a>
+                    <a href="">Instagram</a>
+                    <a href="">Github</a>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="no img yet" />
+            </div> 
+            <div className='flex flex-row'> 
+                <div>
+                    <p>Available For work</p>
+                </div>
+                <div>
+                    <p>Algeria</p>
+                </div>
+            </div>
+            <div>
 
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+                
+            </div>
         </div>
     )
 }
