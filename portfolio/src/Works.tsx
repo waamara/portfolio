8deb9d80ae3469c65ae5f620eb62883209622de9
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-04-16 22:39:58 +0100

    finish for today

--- portfolio/src/Works.tsx
+++ portfolio/src/Works.tsx
@@ -1,3 +1,5 @@
+import Image from "../../portfolio/src/assets/work.png"
+
 const Works = () => {
     return (
 
@@ -6,13 +8,30 @@ const Works = () => {
             backgroundImage: `url($Bgimg)`,
         }}
             className="text-white bg-gray-950 opacity-100  p-8">
-                <div>
-                    <h1>My Selected Projects : </h1>
-                    <p></p>
-                </div>
-                <div>
+            <div>
+                <h1 className="text-2xl font-semibold pb-3 ">My Selected Projects : </h1>
+                <p>Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore</p>
+            </div>
+            <div className=" ">
+                <div className="m-4  rounded-xl bg-[#C2F6FF0F]">
+                    <div>
+                        <img src={Image} alt="Image de  projet " className="w-full p-3" />
+                        <button className="bg-[#0C0F18] rounded-2xl p-2  hidden border-blue-500 border-2  ">Github Repo</button>
+                    </div>
+                    <div className="p-4">
+                        <div>
+                            <p className="text-xl pb-1" >agency landing page</p>
+                            <p>Lorem ipsum dolor sit amet, <br /> consectetur</p>
+                        </div>
+                        <div>
+                            <div>
+                                <p>React Js</p>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
+            </div>
         </div>
     )
 }
