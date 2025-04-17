5425e89a38e7eecbba90f999a2db29af6a9e1ee3
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-04-17 22:41:13 +0100

    finished for today

--- portfolio/src/Contact.tsx
+++ portfolio/src/Contact.tsx
@@ -1,4 +1,7 @@
 import Bgimg from "../../portfolio/src/assets/backgr.png"
+import Linkedin from "../../portfolio/src/assets/email (7).png"
+import Email from "../../portfolio/src/assets/email (1).png"
+import Instagram from "../../portfolio/src/assets/Instagram.png"
 
 
 
@@ -10,34 +13,34 @@ const Contact = () => {
             backgroundImage: `url($Bgimg)`,
         }}
             className="text-white bg-gray-950 opacity-100  p-8">
-                <div>
-                    <p>Let's Connect !</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore</p>
+                <div >
+                    <p className="text-3xl font-semibold pb-3">Let's Connect !</p>
+                    <p className="pb-4"> Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore</p>
                 </div>
                 <div>
-                    <p>Contact</p>
-                    <div>
-                        <img src="" alt="" /> 
+                    <p className="text-xl font-semibold pb-4">Contact</p>
+                    <div className=" flex flex-row">
+                        <img src={Email} alt="email-asset" className="pb-2" /> 
                         <p>berbicheaymen@gmail.com</p>
                     </div>
-                    <div>
-                        <img src="" alt="" /> 
-                        <a href="">Reach me on LinkedIn</a>
+                    <div className=" flex flex-row pb-4">
+                        <img src={Linkedin} alt="Linkedin-asset" /> 
+                        <a href="linkedin.com/in/aymen-berbiche-772a13280">Reach me on LinkedIn</a>
                     </div>
                 </div>
                 <div>
-                    <p>Social Media </p>
-                    <div>
-                        <img src="" alt="" /> 
+                    <p className="text-xl font-semibold pb-4">Social Media </p>
+                    <div className="flex flex-row">
+                        <img src={Instagram} alt="Instagram-icone" /> 
                         <p>visualsbyartam</p>
                     </div>
-                    <div>
-                        <img src="" alt="" /> 
+                    <div className="flex flex-row">
+                    <img src={Instagram} alt="Instagram-icone" /> 
                         <p>visualsbyartam</p>
                     </div>
                 </div>
                 <div>
-                    <input type="text" />
+                    <input type="text"  />
                     <input type="text" />
                     <input type="text" />
                     <button>Send Message </button>
