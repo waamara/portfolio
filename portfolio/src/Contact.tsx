3d2ce0427ca58377db9fd554a71978905c10b36d
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-04-17 22:22:19 +0100

    finished the Contact page content

--- portfolio/src/Contact.tsx
+++ portfolio/src/Contact.tsx
@@ -10,7 +10,38 @@ const Contact = () => {
             backgroundImage: `url($Bgimg)`,
         }}
             className="text-white bg-gray-950 opacity-100  p-8">
-                
+                <div>
+                    <p>Let's Connect !</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore</p>
+                </div>
+                <div>
+                    <p>Contact</p>
+                    <div>
+                        <img src="" alt="" /> 
+                        <p>berbicheaymen@gmail.com</p>
+                    </div>
+                    <div>
+                        <img src="" alt="" /> 
+                        <a href="">Reach me on LinkedIn</a>
+                    </div>
+                </div>
+                <div>
+                    <p>Social Media </p>
+                    <div>
+                        <img src="" alt="" /> 
+                        <p>visualsbyartam</p>
+                    </div>
+                    <div>
+                        <img src="" alt="" /> 
+                        <p>visualsbyartam</p>
+                    </div>
+                </div>
+                <div>
+                    <input type="text" />
+                    <input type="text" />
+                    <input type="text" />
+                    <button>Send Message </button>
+                </div>
         </div>
     )
 }
