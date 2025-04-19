8b213b67614a49b460cf5422e6ac8198e8c561fd
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-04-19 16:19:42 +0100

    padding fixed in the contact form

--- portfolio/src/Contact.tsx
+++ portfolio/src/Contact.tsx
@@ -12,39 +12,39 @@ const Contact = () => {
         <div style={{
             backgroundImage: `url($Bgimg)`,
         }}
-            className="text-white bg-gray-950 opacity-100  p-8">
-                <div >
-                    <p className="text-3xl font-semibold pb-3">Let's Connect !</p>
-                    <p className="pb-4"> Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore</p>
+            className="text-white bg-gray-950 opacity-100  p-8 h-screen">
+            <div >
+                <p className="text-3xl font-semibold pb-3">Let's Connect !</p>
+                <p className="pb-4"> Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore</p>
+            </div>
+            <div>
+                <p className="text-xl font-semibold pb-4">Contact</p>
+                <div className=" flex flex-row">
+                    <img src={Email} alt="email-asset" className="pb-2" />
+                    <p>berbicheaymen@gmail.com</p>
                 </div>
-                <div>
-                    <p className="text-xl font-semibold pb-4">Contact</p>
-                    <div className=" flex flex-row">
-                        <img src={Email} alt="email-asset" className="pb-2" /> 
-                        <p>berbicheaymen@gmail.com</p>
-                    </div>
-                    <div className=" flex flex-row pb-4">
-                        <img src={Linkedin} alt="Linkedin-asset" /> 
-                        <a href="linkedin.com/in/aymen-berbiche-772a13280">Reach me on LinkedIn</a>
-                    </div>
+                <div className=" flex flex-row pb-4 pt-3">
+                    <img src={Linkedin} alt="Linkedin-asset" />
+                    <a href="linkedin.com/in/aymen-berbiche-772a13280">Reach me on LinkedIn</a>
                 </div>
-                <div>
-                    <p className="text-xl font-semibold pb-4">Social Media </p>
-                    <div className="flex flex-row">
-                        <img src={Instagram} alt="Instagram-icone" /> 
-                        <p>visualsbyartam</p>
-                    </div>
-                    <div className="flex flex-row">
-                    <img src={Instagram} alt="Instagram-icone" /> 
-                        <p>visualsbyartam</p>
-                    </div>
+            </div>
+            <div>
+                <p className="text-xl font-semibold pb-4">Social Media </p>
+                <div className="flex flex-row">
+                    <img src={Instagram} alt="Instagram-icone" />
+                    <p>visualsbyartam</p>
                 </div>
-                <div>
-                    <input type="text"  />
-                    <input type="text" />
-                    <input type="text" />
-                    <button>Send Message </button>
+                <div className="flex flex-row pt-4">
+                    <img src={Instagram} alt="Instagram-icone" />
+                    <p >visualsbyartam</p>
                 </div>
+            </div>
+            <div className="flex flex-col mt-12">
+                <input type="text" placeholder="Full Name" className="bg-white" />
+                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Message" />
+                <button>Send Message </button>
+            </div>
         </div>
     )
 }
