4f1f7656701d62293cd771e66b832538047fc3a9
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-04-19 14:00:49 +0100

    added the header informaitons

--- portfolio/src/Header.tsx
+++ portfolio/src/Header.tsx
@@ -3,13 +3,14 @@ import Bgimg from "../../portfolio/src/assets/backgr.png"
 
 const Header  = () => {
     return (
-
-
         <div style={{
             backgroundImage: `url($Bgimg)`,
         }}
             className="text-white bg-gray-950 opacity-100  p-8">
-                
+                <a href="">Home</a> 
+                <a href="">About</a> 
+                <a href="">Works</a> 
+                <a href="">Get in touche</a> 
         </div>
     )
 }
