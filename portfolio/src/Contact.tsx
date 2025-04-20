c342264ad09591a338df2b6d318ccf771ad7e0a5
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-04-20 17:33:18 +0100

    dives fixed for Contact page .tsx

--- portfolio/src/Contact.tsx
+++ portfolio/src/Contact.tsx
@@ -12,38 +12,41 @@ const Contact = () => {
         <div style={{
             backgroundImage: `url($Bgimg)`,
         }}
-            className="text-white bg-[#0A0B10] bg-cover   p-8 h-screen">
-            <div >
-                <p className="text-3xl font-semibold pb-3">Let's Connect !</p>
-                <p className="pb-4"> Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore</p>
+            className="text-white bg-[#0A0B10] bg-cover   p-8 h-full">
+            <div>
+                <div >
+                    <p className="text-3xl font-semibold pb-3">Let's Connect !</p>
+                    <p className="pb-4"> Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore</p>
+                    <div className="flex flex-col mt-12">
+                        <input type="text" placeholder="Full Name" className=" pt-5 pb-5 pl-2 mb-4" />
+                        <input type="text" placeholder="Email" className=" pt-5 pb-5 pl-2 mb-4" />
+                        <input type="text" placeholder="Message" className=" pt-5 pb-5 pl-2 mb-4" />
+                        <button className="bg-[#4D8FCD] rounded-xl  w-[150px] h-[50px]  font-semibold ">Send Message </button>
+                    </div> </div>
             </div>
             <div>
-                <p className="text-xl font-semibold pb-4">Contact</p>
-                <div className=" flex flex-row">
-                    <img src={Email} alt="email-asset" className="pb-2" />
-                    <p>berbicheaymen@gmail.com</p>
+                <div>
+                    <p className="text-xl font-semibold pb-4">Contact</p>
+                    <div className=" flex flex-row">
+                        <img src={Email} alt="email-asset" className="pb-2" />
+                        <p>berbicheaymen@gmail.com</p>
+                    </div>
+                    <div className=" flex flex-row pb-4 pt-3">
+                        <img src={Linkedin} alt="Linkedin-asset" />
+                        <a href="linkedin.com/in/aymen-berbiche-772a13280">Reach me on LinkedIn</a>
+                    </div>
                 </div>
-                <div className=" flex flex-row pb-4 pt-3">
-                    <img src={Linkedin} alt="Linkedin-asset" />
-                    <a href="linkedin.com/in/aymen-berbiche-772a13280">Reach me on LinkedIn</a>
+                <div>
+                    <p className="text-xl font-semibold pb-4">Social Media </p>
+                    <div className="flex flex-row">
+                        <img src={Instagram} alt="Instagram-icone" />
+                        <p>visualsbyartam</p>
+                    </div>
+                    <div className="flex flex-row pt-4">
+                        <img src={Instagram} alt="Instagram-icone" />
+                        <p >visualsbyartam</p>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <p className="text-xl font-semibold pb-4">Social Media </p>
-                <div className="flex flex-row">
-                    <img src={Instagram} alt="Instagram-icone" />
-                    <p>visualsbyartam</p>
-                </div>
-                <div className="flex flex-row pt-4">
-                    <img src={Instagram} alt="Instagram-icone" />
-                    <p >visualsbyartam</p>
-                </div>
-            </div>
-            <div className="flex flex-col mt-12">
-                <input type="text" placeholder="Full Name" className=" pt-5 pb-5 pl-2 mb-4" />
-                <input type="text" placeholder="Email" className=" pt-5 pb-5 pl-2 mb-4" />
-                <input type="text" placeholder="Message" className=" pt-5 pb-5 pl-2 mb-4" />
-                <button className="bg-[#4D8FCD] rounded-xl  w-[150px] h-[50px]  font-semibold ">Send Message </button>
             </div>
         </div>
     )
