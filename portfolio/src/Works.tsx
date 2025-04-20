8d79d06dd71a11133c60a955ecbc0f2ee971ee31
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-04-20 17:29:05 +0100

    now fixed for works

--- portfolio/src/Works.tsx
+++ portfolio/src/Works.tsx
@@ -12,7 +12,7 @@ const Works = () => {
                 <h1 className="text-2xl font-semibold pb-3 ">My Selected Projects : </h1>
                 <p>Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore</p>
             </div>
-            <div className="   grid grid-cols-3 ">
+            <div className="   md:grid md:grid-cols-3 ">
                 <div className="m-4  rounded-xl bg-[#C2F6FF0F]">
                     <div>
                         <img src={Image} alt="Image de  projet " className="w-full p-3" />
