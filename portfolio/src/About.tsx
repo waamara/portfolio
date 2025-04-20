ae777486acf8648381f9c410d1ffe110dd0ddd88
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-04-20 17:08:18 +0100

    first devision of first div is done

--- portfolio/src/About.tsx
+++ portfolio/src/About.tsx
@@ -17,13 +17,15 @@ const About = () => {
         }}
             className="text-white bg-gray-950 opacity-100  p-8">
             <div>
-                <p className="text-3xl">I Am Aymen </p> <br />
-                <p>I'm Aymen, a passionate frontend developer and a computer science student at USTHB. I specialize in building modern, responsive, and user-friendly web applications using the latest technologies.</p>
-                <br />
-                <p>Currently, I’m the Web Development Team Lead at Open Minds Club, where I collaborate with talented developers to create impactful digital experiences.</p>
-                <br />
                 <div className="p-12">
                     <img src={Mypic} alt="My-img " />
+                </div>
+                <div>
+                    <p className="text-3xl">I Am Aymen </p> <br />
+                    <p>I'm Aymen, a passionate frontend developer and a computer science student at USTHB. I specialize in building modern, responsive, and user-friendly web applications using the latest technologies.</p>
+                    <br />
+                    <p>Currently, I’m the Web Development Team Lead at Open Minds Club, where I collaborate with talented developers to create impactful digital experiences.</p>
+                    <br />
                 </div>
                 <div className=" flex flex-col p-6 mt-10 items-center h-[200px]  bg-[#C2F6FF08] rounded-2xl">
                     <div className="flex flex-row">
