faca24e3eed5aff64a1d323e42146f678362909e
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-07-07 23:29:22 +0100

    added a new file with content

---
+++ portfolio/src/components/CardRotate.tsx
@@ -0,0 +1,44 @@
+// src/components/CardRotate.tsx
+import React from 'react';
+import { motion, useMotionValue, useTransform, PanInfo } from "framer-motion";
+
+interface CardRotateProps {
+    children: React.ReactNode;
+    onSendToBack: () => void;
+    sensitivity: number;
+}
+
+const CardRotate: React.FC<CardRotateProps> = ({ children, onSendToBack, sensitivity }) => {
+    const x = useMotionValue(0);
+    const y = useMotionValue(0);
+    const rotateX = useTransform(y, [-100, 100], [60, -60]);
+    const rotateY = useTransform(x, [-100, 100], [-60, 60]);
+
+    function handleDragEnd(_: any, info: PanInfo) {
+        if (
+            Math.abs(info.offset.x) > sensitivity ||
+            Math.abs(info.offset.y) > sensitivity
+        ) {
+            onSendToBack();
+        } else {
+            x.set(0);
+            y.set(0);
+        }
+    }
+
+    return (
+        <motion.div
+            className="absolute cursor-grab"
+            style={{ x, y, rotateX, rotateY }}
+            drag
+            dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            dragElastic={0.6}
+            whileTap={{ cursor: "grabbing" }}
+            onDragEnd={handleDragEnd}
+        >
+            {children}
+        </motion.div>
+    );
+};
+
+export default CardRotate;
