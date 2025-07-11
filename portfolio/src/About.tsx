ad086ce9b1f367ababd0a641073a91d36e76c75e
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-07-11 17:43:19 +0100

    content done

--- portfolio/src/About.tsx
+++ portfolio/src/About.tsx
@@ -90,8 +90,12 @@ const about = () => {
                             <p className="text-xl pl-8 font-semibold">Available For work</p>
                         </div>
                     </div>
-                    <div className="flex flex-row ">
-                        <div className=" p-5 w-[120px] h-[100px] flex flex-col  items-center bg-[#EAF4FF08] border-[1px]  hover:bg-gray-950 border-[#A1CDFF14]  rounded-2xl ">
+                    <div className="flex flex-row  pt-4">
+                        <div className=" p-5 w-[140px] h-[100px] flex flex-col  items-center bg-[#EAF4FF08] border-[1px]  hover:bg-gray-950 border-[#A1CDFF14]  rounded-2xl ">
+                            <p>See My <br /> Resume</p>
+                            <img src={Ficone} alt="icone" className="flex h-3 w-3 " />
+                        </div>
+                        <div className=" p-5 w-[140px] h-[100px] flex flex-col  items-center bg-[#EAF4FF08] border-[1px]  hover:bg-gray-950 border-[#A1CDFF14]  rounded-2xl ">
                             <p>See My <br /> Resume</p>
                             <img src={Ficone} alt="icone" className="flex h-3 w-3 " />
                         </div>
