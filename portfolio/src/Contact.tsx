ed189fb2cdac69f717d01df795cb1606f2821a09
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-07-16 19:41:57 +0100

    commit changes

--- portfolio/src/Contact.tsx
+++ portfolio/src/Contact.tsx
@@ -1,15 +1,19 @@
 import  Location from "../src/assets/Cash Pin Map--Streamline-Ultimate (1).png" 
+import Phone from "../src/assets/Phone Actions Receive--Streamline-Ultimate.png"
+import Email from "../src/assets/Envelope--Streamline-Ultimate.png"
 
 const Contact = () => {
     return (
         <div style={{
             backgroundImage: `url($Bgimg)`,
         }}
-            className="text-white bg-[#0A0B10] bg-cover md:flex md:flex-row md:justify-around p-8 h-svh">
-            <div className="bg-[#EAF4FF08] p-12 border-1 rounded-2xl border-[#A1CDFF14] ">
+            className="text-white bg-[#0A0B10] flex flex-row justify-evenly p-8 h-screen ">
+            
+            <div className="bg-[#EAF4FF08] p-12 border-1 rounded-2xl border-[#A1CDFF14] h-[500px] w-[600px] ">
+                
                 <div >
                     <p className="text-3xl font-semibold pb-3">Let's Connect !</p>
-                    <p className="pb-4"> Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore</p>
+                    <p className="pb-4"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br /> eiusmod tempor incididunt ut labore</p>
                     <div className="flex flex-col mt-12">
                         <div className="flex flex-col md:flex-row ">
                             <input type="text" placeholder="Full Name" className=" pt-5 pb-5 pl-2 mb-4" />
@@ -25,17 +29,17 @@ const Contact = () => {
                     <p>I'm available across multiple channels. Feel <br /> free to connect however it suits you best.</p>
                     <div className=" flex flex-row space-x-3 mt-4 border-b-1 border-b-[#55A4FF] p-2">
                         <p>Location</p>
-                        <img src={Location} alt="" />
+                        <img src={Location} alt="Location Icone" />
                         <p>Algiers, Algeria</p>
                     </div>
                     <div className=" flex flex-row space-x-3 mt-4 border-b-1 border-b-[#55A4FF] p-2">
                         <p>Email</p>
-                        <img src={Location} alt="" />
+                        <img src={Email} alt="Email icone" />
                         <p>berbicheaymen@gmail.com</p>
                     </div>
                     <div className=" flex flex-row space-x-3 mt-4 border-b-1 border-b-[#55A4FF] p-2">
                         <p>Phone</p>
-                        <img src={Location} alt="" />
+                        <img src={Phone} alt="Phone icone" />
                         <p>06588300028</p>
                     </div>
                 </div>
