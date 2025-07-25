b1ff3afb45ace674e1759a554c165df5668e6524
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-07-25 16:22:33 +0100

    commit changes

--- portfolio/src/testemonial.ts
+++ portfolio/src/testemonial.ts
@@ -1,5 +1,6 @@
 import Mypic from "./assets/me.jpg"
 import LD from "../src/assets/photo_5906813104682028146_y.jpg"
+import AA from "../src/assets/image0.jpg"
 
 export interface Testimonial {
     id: number;
@@ -23,6 +24,13 @@ const testimonials: Testimonial[] = [
         role: "Frontend Developer",
         content: "Working with ACC has been a great experience. He’s a skilled and dependable web developer who consistently delivers high-quality work. His professionalism, problem-solving ability, and team spirit made him a valuable part of every project",
         avatar: LD
+    },
+    {
+        id:3, 
+        name: "Abderrahmane LEMMOUCHI", 
+        role: "CS student and creator of Devshroom club 🍄",
+        content:"Working with Aymen has been a truly rewarding experience.  What sets him apart, however, is his exceptional communication and tutoring abilities. He explains complex concepts with clarity and patience, making collaboration easy and productive. Aymen is a great team player and mentor.",
+        avatar:AA
     },
 ];
 
