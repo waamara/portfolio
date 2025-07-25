3ab9652abca6aedeceaad0892018b844836bb36c
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-07-25 18:36:36 +0100

    commit changes

--- portfolio/src/Header.tsx
+++ portfolio/src/Header.tsx
@@ -9,19 +9,19 @@ const Header = () => {
                 <div className="flex gap-10">
                     <NavLink 
                         to="/" 
-                        className={({ isActive }) => isActive ? "font-semibold text-amber-300" : "text-white"}
+                        className={({ isActive }) => isActive ? "font-semibold text-shadow-gray-400" : "text-white"}
                     >
                         Home
                     </NavLink>
                     <NavLink 
                         to="/about" 
-                        className={({ isActive }) => isActive ? "font-semibold text-red-500" : "text-white"}
+                        className={({ isActive }) => isActive ? "font-semibold text-shadow-gray-400" : "text-white"}
                     >
                         About
                     </NavLink>
                     <NavLink 
                         to="/works" 
-                        className={({ isActive }) => isActive ? " font-semibold text-red-500" : "text-white"}
+                        className={({ isActive }) => isActive ? " font-semibold text-shadow-gray-400" : "text-white"}
                     >
                         Works
                     </NavLink>
@@ -29,7 +29,7 @@ const Header = () => {
                 <div className="flex flex-row justify-center text-center gap-4">
                     <NavLink 
                         to="/contact" 
-                        className={({ isActive }) => isActive ? "font-semibold text-red-500" : "text-white"}
+                        className={({ isActive }) => isActive ? "font-semibold text-shadow-gray-400" : "text-white"}
                     >
                         Get in Touch
                     </NavLink>
