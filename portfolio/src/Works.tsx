61717848d325ff81ec2d5caebea61e71dece5b5b
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-07-25 18:23:47 +0100

    commit

--- portfolio/src/Works.tsx
+++ portfolio/src/Works.tsx
@@ -4,13 +4,13 @@ const Works = () => {
     return (
 
 
-        <div 
+        <div
             className="text-white bg-[#0F0F0F]   p-8">
             <div className="ml-8 mt-8">
                 <h1 className="text-2xl font-semibold pb-3 ">My Selected Projects : </h1>
                 <p>Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore</p>
             </div>
-            <hr className="text-[#A1CDFF1A] ml-8 mr-8 mt-8"  />
+            <hr className="text-[#A1CDFF1A] ml-8 mr-8 mt-8" />
             <div className="   md:grid md:grid-cols-3 ">
                 <div className="m-4  rounded-xl ">
                     <div>
@@ -24,15 +24,15 @@ const Works = () => {
                         </div>
                         <div>
                             <div className="flex flex-row  space-x-4 mt-4">
-                                <p className="bg-[#3F82FF1C] border-1 border-[#A1CDFF14] rounded-xl p-3  ">React Js</p> 
+                                <p className="bg-[#3F82FF1C] border-1 border-[#A1CDFF14] rounded-xl p-3  ">React Js</p>
                                 <p className="bg-[#3F82FF1C] rounded-xl border-1 border-[#A1CDFF14] p-3  ">Tailwind Css</p>
                                 <p className="bg-[#3F82FF1C] rounded-xl border-1 border-[#A1CDFF14] p-3 ] ">Next Js</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                
-                 <div className="m-4  rounded-xl ">
+
+                <div className="m-4  rounded-xl ">
                     <div>
                         <img src={Image} alt="Image de  projet " className="w-full p-3" />
                         <button className="bg-[#0C0F18] rounded-2xl p-2  hidden border-blue-500 border-2  ">Github Repo</button>
@@ -44,14 +44,14 @@ const Works = () => {
                         </div>
                         <div>
                             <div className="flex flex-row  space-x-4 mt-4">
-                                <p className="bg-[#3F82FF1C] border-1 border-[#A1CDFF14] rounded-xl p-3  ">React Js</p> 
+                                <p className="bg-[#3F82FF1C] border-1 border-[#A1CDFF14] rounded-xl p-3  ">React Js</p>
                                 <p className="bg-[#3F82FF1C] rounded-xl border-1 border-[#A1CDFF14] p-3  ">Tailwind Css</p>
                                 <p className="bg-[#3F82FF1C] rounded-xl border-1 border-[#A1CDFF14] p-3 ] ">Next Js</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                 <div className="m-4  rounded-xl ">
+                <div className="m-4  rounded-xl ">
                     <div>
                         <img src={Image} alt="Image de  projet " className="w-full p-3" />
                         <button className="bg-[#0C0F18] rounded-2xl p-2  hidden border-blue-500 border-2  ">Github Repo</button>
@@ -63,14 +63,14 @@ const Works = () => {
                         </div>
                         <div>
                             <div className="flex flex-row  space-x-4 mt-4">
-                                <p className="bg-[#3F82FF1C] border-1 border-[#A1CDFF14] rounded-xl p-3  ">React Js</p> 
+                                <p className="bg-[#3F82FF1C] border-1 border-[#A1CDFF14] rounded-xl p-3  ">React Js</p>
                                 <p className="bg-[#3F82FF1C] rounded-xl border-1 border-[#A1CDFF14] p-3  ">Tailwind Css</p>
                                 <p className="bg-[#3F82FF1C] rounded-xl border-1 border-[#A1CDFF14] p-3 ] ">Next Js</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                 <div className="m-4  rounded-xl ">
+                <div className="m-4  rounded-xl ">
                     <div>
                         <img src={Image} alt="Image de  projet " className="w-full p-3" />
                         <button className="bg-[#0C0F18] rounded-2xl p-2  hidden border-blue-500 border-2  ">Github Repo</button>
@@ -82,14 +82,14 @@ const Works = () => {
                         </div>
                         <div>
                             <div className="flex flex-row  space-x-4 mt-4">
-                                <p className="bg-[#3F82FF1C] border-1 border-[#A1CDFF14] rounded-xl p-3  ">React Js</p> 
+                                <p className="bg-[#3F82FF1C] border-1 border-[#A1CDFF14] rounded-xl p-3  ">React Js</p>
                                 <p className="bg-[#3F82FF1C] rounded-xl border-1 border-[#A1CDFF14] p-3  ">Tailwind Css</p>
                                 <p className="bg-[#3F82FF1C] rounded-xl border-1 border-[#A1CDFF14] p-3 ] ">Next Js</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                 <div className="m-4  rounded-xl ">
+                <div className="m-4  rounded-xl ">
                     <div>
                         <img src={Image} alt="Image de  projet " className="w-full p-3" />
                         <button className="bg-[#0C0F18] rounded-2xl p-2  hidden border-blue-500 border-2  ">Github Repo</button>
@@ -101,14 +101,14 @@ const Works = () => {
                         </div>
                         <div>
                             <div className="flex flex-row  space-x-4 mt-4">
-                                <p className="bg-[#3F82FF1C] border-1 border-[#A1CDFF14] rounded-xl p-3  ">React Js</p> 
+                                <p className="bg-[#3F82FF1C] border-1 border-[#A1CDFF14] rounded-xl p-3  ">React Js</p>
                                 <p className="bg-[#3F82FF1C] rounded-xl border-1 border-[#A1CDFF14] p-3  ">Tailwind Css</p>
                                 <p className="bg-[#3F82FF1C] rounded-xl border-1 border-[#A1CDFF14] p-3 ] ">Next Js</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                 <div className="m-4  rounded-xl ">
+                <div className="m-4  rounded-xl ">
                     <div>
                         <img src={Image} alt="Image de  projet " className="w-full p-3" />
                         <button className="bg-[#0C0F18] rounded-2xl p-2  hidden border-blue-500 border-2  ">Github Repo</button>
@@ -120,16 +120,16 @@ const Works = () => {
                         </div>
                         <div>
                             <div className="flex flex-row  space-x-4 mt-4">
-                                <p className="bg-[#3F82FF1C] border-1 border-[#A1CDFF14] rounded-xl p-3  ">React Js</p> 
+                                <p className="bg-[#3F82FF1C] border-1 border-[#A1CDFF14] rounded-xl p-3  ">React Js</p>
                                 <p className="bg-[#3F82FF1C] rounded-xl border-1 border-[#A1CDFF14] p-3  ">Tailwind Css</p>
                                 <p className="bg-[#3F82FF1C] rounded-xl border-1 border-[#A1CDFF14] p-3 ] ">Next Js</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                
+
             </div>
-             <hr className="text-[#A1CDFF1A] mt-10 ml-8 mr-8" />
+            <hr className="text-[#A1CDFF1A] mt-10 ml-8 mr-8" />
             <div className="flex justify-center items-center mt-8  ">
                 <p> Made By Aymen  &copy; {new Date().getFullYear()}   </p>
             </div>
