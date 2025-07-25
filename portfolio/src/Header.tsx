a3a50399eb746359cbb0921d63a9e3c55638536c
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-07-25 18:34:39 +0100

    commit changes

--- portfolio/src/Header.tsx
+++ portfolio/src/Header.tsx
@@ -1,25 +1,42 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Bgimg from "../src/assets/backgr.png";
-import Icone from "../src/assets/icon.png"
+import Icone from "../src/assets/icon.png";
 
 const Header = () => {
     return (
-        <div
-            className="text-white bg-[#0F0F0F] pt-10 pl-18 pr-20"
-        >
-            <nav className="flex gap-4 justify-between  ">
+        <div className="text-white bg-[#0F0F0F] pt-10 pl-18 pr-20">
+            <nav className="flex gap-4 justify-between">
                 <div className="flex gap-10">
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/works">Works</Link>
+                    <NavLink 
+                        to="/" 
+                        className={({ isActive }) => isActive ? "font-semibold text-amber-300" : "text-white"}
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink 
+                        to="/about" 
+                        className={({ isActive }) => isActive ? "font-semibold text-red-500" : "text-white"}
+                    >
+                        About
+                    </NavLink>
+                    <NavLink 
+                        to="/works" 
+                        className={({ isActive }) => isActive ? " font-semibold text-red-500" : "text-white"}
+                    >
+                        Works
+                    </NavLink>
                 </div>
                 <div className="flex flex-row justify-center text-center gap-4">
-                    <Link to="/contact">Get in Touch</Link>
-                    <img src={Icone} alt="icone " className="w-[20px] h-[20px]" />
+                    <NavLink 
+                        to="/contact" 
+                        className={({ isActive }) => isActive ? "font-semibold text-red-500" : "text-white"}
+                    >
+                        Get in Touch
+                    </NavLink>
+                    <img src={Icone} alt="icon" className="w-[20px] h-[20px]" />
                 </div>
             </nav>
         </div>
-        
     );
 };
 
