b782888ea86ed8740897fc75bd75f436900c60aa
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-11-09 21:47:07 +0100

    s

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // ✅ Required for GitHub Pages
   plugins: [
     tailwindcss(),
-    react()  
-  ],
+    react()
+  ]
 })
