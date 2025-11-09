8382a9c3077bdbe844ef3feb493a04b2ab281aa7
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-11-09 21:51:51 +0100

    a

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/portfolio">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
